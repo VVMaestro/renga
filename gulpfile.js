0982782dcf2c7d6af1498e2356421480233867e1
Valentine <bmaecmpo@gmail.com>
2018-12-22 22:59:55 +0500

    Hello world

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("style", function () {
+  return gulp.src("source/css/style.css")
+    .pipe(plumber())    
+    .pipe(server.stream());
+});
+
+gulp.task("serve", function () {
+  server.init({
+    server: "source/"
+  });
+
+  gulp.watch("source/css/style.css", gulp.series("style"));
+  gulp.watch("source/*.html").on("change", server.reload);
+});
